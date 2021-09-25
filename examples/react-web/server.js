0d37b89f07531799a2d191728a48b84ddeefa5fc
casualbrooks <brooks@designiscasual.com>
2021-09-25 15:21:53 -0600

    adds heroku server connection

--- examples/react-web/server.js
+++ examples/react-web/server.js
@@ -1,20 +1,7 @@
-/*
- * Copyright 2017 The boardgame.io Authors
- *
- * Use of this source code is governed by a MIT-style
- * license that can be found in the LICENSE file or at
- * https://opensource.org/licenses/MIT.
- */
-
 import { Server, Origins } from 'boardgame.io/server';
 import TicTacToe from './src/tic-tac-toe/game';
-import Chess from './src/chess/game';
 
+const server = Server({ games: [TicTacToe], origins: [Origins.LOCALHOST] });
 const PORT = process.env.PORT || 8000;
-const server = Server({
-  games: [TicTacToe, Chess],
-  origins: [Origins.LOCALHOST],
-});
-server.run(PORT, () => {
-  console.log(`Serving at: http://localhost:${PORT}`);
-});
+
+server.run(PORT);
